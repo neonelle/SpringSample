73b167094d2d0899563c8c9efa48aaac61d5f181
elleneon <nhupkbe_jtpwjge@yahoo.co.jp>
2019-10-28 21:48:57 +0900

    20191028

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,7 @@
+/* 従業員テーブルのデータ */
 INSERT INTO employee (employee_id, employee_name, age)
-VALUES(1, 'yamada taro', 30);
+VALUES(1, '山田太郎', 30);
 
-INSERT INTO employee (employee_id, employee_name, age)
-VALUES(2, 'aiueo', 10);
+/* ユーザーマスタのデータ（ADMIN権限） */
+INSERT INTO m_user (user_id, password, user_name, birthday, age, marriage, role)
+VALUES('yamada@xxx.co.jp', 'password', '山田太郎', '1990-01-01', 28, false, 'ROLE_ADMIN');
